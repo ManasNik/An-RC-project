ec8eede2c85f8a2c7c0872f8fd6c29f32f36f5b7
Manas Nikhil <manasnikhil007@gmail.com>
2025-07-18 00:09:18 +0530

    Initial commit: Car code

---
+++ CAR FIRMWARE/Core/Inc/TB6612FNG.h
@@ -0,0 +1,66 @@
+#ifndef _TB6612FNG_H
+#define _TB6612FNG_H
+
+#include "stdbool.h"
+// I am using two drivers, one for the two motors on the front and the other for the two in the rear.
+
+/* Motor driver typedef struct to contain all necessary hardware peripherals in one place.
+ * Both motor drivers are included in the same struct.
+*/
+typedef struct{
+	// Standby pins
+	GPIO_TypeDef* stdby_front_port;
+	uint16_t stdby_front_pin;
+
+	GPIO_TypeDef* stdby_rear_port;
+	uint16_t stdby_rear_pin;
+
+	/* IN1 and IN2 pins for each motor. These pins determine direction of motor rotation.
+	 * Each motor has two pairs of IN1 and IN2 pins denoted by A and B.
+	*/
+
+	// Front left
+	GPIO_TypeDef* AIN1_front_port;
+	uint16_t AIN1_front_pin;
+
+	GPIO_TypeDef* AIN2_front_port;
+	uint16_t AIN2_front_pin;
+
+	// Front right
+	GPIO_TypeDef* BIN1_front_port;
+	uint16_t BIN1_front_pin;
+
+	GPIO_TypeDef* BIN2_front_port;
+	uint16_t BIN2_front_pin;
+
+	// Rear left
+	GPIO_TypeDef* AIN1_rear_port;
+	uint16_t AIN1_rear_pin;
+
+	GPIO_TypeDef* AIN2_rear_port;
+	uint16_t AIN2_rear_pin;
+
+	// Rear right
+	GPIO_TypeDef* BIN1_rear_port;
+	uint16_t BIN1_rear_pin;
+
+	GPIO_TypeDef* BIN2_rear_port;
+	uint16_t BIN2_rear_pin;
+
+	// Using the same timer peripheral for generating PWM for all four motors.
+	TIM_HandleTypeDef* htim;
+
+	// Timer channels
+	uint32_t front_left;
+	uint32_t front_right;
+	uint32_t rear_left;
+	uint32_t rear_right;
+
+}tb6612fng_t;
+
+// Function declarations
+void tb6612fng_init(tb6612fng_t* tbf);
+void tb6612fng_direction(tb6612fng_t* tbf, bool in1, bool in2);
+void tb6612fng_speed(tb6612fng_t tbf ,uint16_t speed_left, uint16_t speed_right);
+
+#endif
---
+++ CAR FIRMWARE/Core/Src/main.c
@@ -0,0 +1,428 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdbool.h"
+#include "TB6612FNG.h"
+#include "NRF24L01.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
+TIM_HandleTypeDef htim1;
+
+/* USER CODE BEGIN PV */
+tb6612fng_t motor_driver; // Custom struct for all motor driver pins and PWM channels
+
+uint16_t speed_left;
+uint16_t speed_right;
+
+bool in1 = true; // CW or forward by default
+bool in2 = false;
+
+uint8_t RX_address[] = {0xEE,0xDD,0xCC,0xBB,0xAA};
+uint8_t RX_buffer[5];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_TIM1_Init(void);
+/* USER CODE BEGIN PFP */
+void tb6612fng_config(tb6612fng_t* tbf);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_TIM1_Init();
+  /* USER CODE BEGIN 2 */
+  tb6612fng_config(&motor_driver); // Assign values to struct attributes
+  tb6612fng_init(&motor_driver);
+
+  nrf24_init();
+  nrf24_RXmode(RX_address, 5, 10);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  if(isDataAvailable(1)){
+		  nrf24_Receive(RX_buffer,5);
+	  }
+	  // Note: Data in RX_buffer follows the same indexing as TX data buffer.
+	  if((bool)RX_buffer[0] == 0){
+		  in1 = false;
+		  in2 = true;
+	  }
+	  else{
+		  in1 = true;
+		  in2 = false;
+	  }
+
+	  // Reconstruction of the 16-bit speed values from the two received 8-bit data pairs.
+	  speed_left = ((uint16_t) RX_buffer[1] << 8) | RX_buffer[2];
+	  speed_right = ((uint16_t) RX_buffer[3] << 8) | RX_buffer[4];
+
+	  // Forward or reverse control
+	  tb6612fng_direction(&motor_driver, in1, in2);
+
+	  // Control the speed and steering
+	  tb6612fng_speed(motor_driver, speed_left, speed_right);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 1000-1;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, CSS_Pin|CE_Pin|LED_Pin|STBY_1_Pin
+                          |STBY_2_Pin|AIN1_1_Pin|AIN2_1_Pin|BIN1_1_Pin
+                          |BIN2_1_Pin|AIN2_2_Pin|BIN1_2_Pin|BIN2_2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(AIN1_2_GPIO_Port, AIN1_2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : CSS_Pin CE_Pin LED_Pin STBY_1_Pin
+                           STBY_2_Pin AIN1_1_Pin AIN2_1_Pin BIN1_1_Pin
+                           BIN2_1_Pin AIN2_2_Pin BIN1_2_Pin BIN2_2_Pin */
+  GPIO_InitStruct.Pin = CSS_Pin|CE_Pin|LED_Pin|STBY_1_Pin
+                          |STBY_2_Pin|AIN1_1_Pin|AIN2_1_Pin|BIN1_1_Pin
+                          |BIN2_1_Pin|AIN2_2_Pin|BIN1_2_Pin|BIN2_2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : AIN1_2_Pin */
+  GPIO_InitStruct.Pin = AIN1_2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(AIN1_2_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+// Instance of the motor driver struct
+	void tb6612fng_config(tb6612fng_t* tbf){
+
+		// Number after the underscore represents the front(1)/rear(2) motor driver.
+		tbf->stdby_front_port = STBY_1_GPIO_Port;
+		tbf->stdby_front_pin = STBY_1_Pin;
+
+		tbf->stdby_rear_port = STBY_2_GPIO_Port;
+		tbf->stdby_rear_pin = STBY_2_Pin;
+
+		// Front left
+		tbf->AIN1_front_port = AIN1_1_GPIO_Port;
+		tbf->AIN1_front_pin = AIN1_1_Pin;
+
+		tbf->AIN2_front_port = AIN2_1_GPIO_Port;
+		tbf->AIN2_front_pin = AIN2_1_Pin;
+
+		// Front right
+		tbf->BIN1_front_port = BIN1_1_GPIO_Port;
+		tbf->BIN1_front_pin = BIN1_1_Pin;
+
+		tbf->BIN2_front_port = BIN2_1_GPIO_Port;
+		tbf->BIN2_front_pin = BIN2_1_Pin;
+
+		// Rear left
+		tbf->AIN1_rear_port = AIN1_2_GPIO_Port;
+		tbf->AIN1_rear_pin = AIN1_2_Pin;
+
+		tbf->AIN2_rear_port = AIN2_2_GPIO_Port;
+		tbf->AIN2_rear_pin = AIN2_2_Pin;
+
+		// Rear right
+		tbf->BIN1_rear_port = BIN1_2_GPIO_Port;
+		tbf->BIN1_rear_pin = BIN1_2_Pin;
+
+		tbf->BIN2_rear_port = BIN2_2_GPIO_Port;
+		tbf->BIN2_rear_pin = BIN2_2_Pin;
+
+		tbf->htim = &htim1;
+
+		tbf->front_left = TIM_CHANNEL_1;
+		tbf->front_right = TIM_CHANNEL_2;
+		tbf->rear_left = TIM_CHANNEL_3;
+		tbf->rear_right = TIM_CHANNEL_4;
+
+	}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
